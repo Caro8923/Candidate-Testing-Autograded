29ca28d969301423da2c4affd2252cf6c795ea23
Carolyn Smith <carolynsmith@slu.edu>
2024-06-15 19:37:06 -0500

    Completed Task 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -56,8 +56,6 @@ function runProgram() {
   gradeQuiz(this.candidateAnswers);
 }
 
-runProgram()
-
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
