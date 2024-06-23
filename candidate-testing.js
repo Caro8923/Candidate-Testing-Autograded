4f1159bdf1437e3af8538d93a4cd7b03e29a8848
Carolyn Smith <carolynsmith@slu.edu>
2024-06-22 22:22:31 -0500

    Candidate Testing Task 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,10 +12,20 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
 
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
+let candidateAnswers =[];
+
+for (i = 0; i < questions.length; i++) {
+  candidateAnswers = input.question(questions[i])
+  console.log(`Your answer was ${candidateAnswers} and the correct answer is ${correctAnswers[i]}`)
+}
+
+/*
+for (i=0; i < questions.length; i++) {
+  console.log(`Your answer was ${candidateAnswers[i]} and the correct answer is ${correctAnswers[i]}`);
+}*/
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
